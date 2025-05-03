8ab7090e40130bf1aaecf7af31e8defea536d348
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2025-05-02 23:39:26 -0300

    feat:criando metricas de logins mal e bem sucedidos

--- src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument;
 
 import br.com.alura.forum.config.security.TokenService;
 import br.com.alura.forum.controller.dto.TokenDto;
@@ -22,6 +24,19 @@ import br.com.alura.forum.controller.form.LoginForm;
 @RequestMapping("/auth")
 @Profile(value = {"prod", "test"})
 public class AutenticacaoController {
+
+	Counter authUserSuccess;
+	Counter authUserErrors;
+
+	public AutenticacaoController(MeterRegistry registry) {
+		authUserSuccess = Counter.builder("auth_user_success")
+				.description("usuarios autenticados")
+				.register(registry);
+
+		authUserErrors = Counter.builder("auth_user_error")
+				.description("erros de login")
+				.register(registry);
+	}
 	
 	@Autowired
 	private AuthenticationManager authManager;
@@ -35,10 +50,12 @@ public class AutenticacaoController {
 		
 		try {
 			Authentication authentication = authManager.authenticate(dadosLogin);
-			String token = tokenService.gerarToken(authentication); 		
+			String token = tokenService.gerarToken(authentication);
+			authUserSuccess.increment()
 			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
 			
 		} catch (AuthenticationException e) {
+			authUserErrors.increment()
 			return ResponseEntity.badRequest().build();
 		}
 
